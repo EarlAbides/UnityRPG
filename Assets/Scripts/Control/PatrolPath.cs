b13d082fc923dd1584c2a5d340110d9a03c787ab
Jeffrey Earl <jeffreyearl@hotmail.com>
2024-03-23 15:58:34 -0400

    52 waypoint loops

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -5,13 +5,24 @@ namespace RPG.Control
     public class PatrolPath : MonoBehaviour
     {
         const float gizmoWaypointRadius = 0.2f;
-        
+
         void OnDrawGizmos() {
             for (int i = 0; i < transform.childCount; i++)
             {
                 Gizmos.color = Color.gray;
-                Gizmos.DrawSphere(transform.GetChild(i).position, gizmoWaypointRadius);
+                Gizmos.DrawSphere(GetWaypoint(i), gizmoWaypointRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(GetNextIndex(i)));
             }
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+
+        private int GetNextIndex(int i) 
+        {
+            return (i < transform.childCount - 1) ? i + 1 : 0;
         }
     }
 }
