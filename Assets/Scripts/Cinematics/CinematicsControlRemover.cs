7450bc63bd21b9ee98a3d89b24a9b7209dbe1382
Jeffrey Earl <jeffreyearl@hotmail.com>
2024-03-25 23:00:05 -0400

    66 control in cutscenes

---
+++ Assets/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -0,0 +1,30 @@
+using RPG.Control;
+using RPG.Core;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicsControlRemover : MonoBehaviour
+    {
+        GameObject player;
+
+        void Start()
+        {
+            player = GameObject.FindWithTag("Player");
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+        }
+
+        void DisableControl(PlayableDirector playableDirector)
+        {
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+
+        void EnableControl(PlayableDirector playableDirector)
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+    }
+}
--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -81,6 +81,7 @@ namespace RPG.Combat
         public void Cancel()
         {
             StopAttack();
+            mover.Cancel();
             target = null;
         }
 
