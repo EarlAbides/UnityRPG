6a2ee33083a7fdf1d1f57d834f429f72121fdfad
Jeffrey Earl <jeffreyearl@hotmail.com>
2024-03-27 00:14:54 -0400

    71 wait for scene

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                print("Loading scene!");
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            print("Scene loaded");
+            Destroy(gameObject);
+        }
+    }
+}
